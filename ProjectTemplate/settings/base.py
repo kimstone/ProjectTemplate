84c858965e24c17ca18703a177be890474f78251
Kim Stone <techmate@Mac-mini.localdomain>
2021-04-25 19:08:51 -0400

    installed Django Debug Toolbar

--- ProjectTemplate/settings/base.py
+++ ProjectTemplate/settings/base.py
@@ -11,6 +11,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'debug_toolbar',
 ]
 
 PROJECT_APPS = [
--- ProjectTemplate/urls.py
+++ ProjectTemplate/urls.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -7,3 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing_page, name='landing_page')
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),]
